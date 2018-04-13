93cbb7cc64a6144d7c4973734df504f2d66305af
i.n.g.o <ingo.randolf@servus.at>
2018-04-13 15:55:01 +0200

    add kicad-project, PAN-writer and PAN-arduino-read

---
+++ PAN-arduino-read/PAN-arduino-read.ino
@@ -0,0 +1,428 @@
+#include <Packetizer.h>
+/*
+ * using Packetizer
+ * https://github.com/i-n-g-o/Packetizer
+ */
+#include <RingBuffer.h>
+/*
+ * using BufferUtils by Christopher Baker:
+ * https://github.com/bakercp/BufferUtils
+ */
+
+
+#define PARITY_ODD;
+/*
+ * why ODD:
+ * only zero-value should result in a non-valid byte
+ * this is only possible with odd parity
+ */
+
+ #define IS_FLIPPED
+ /*
+  * input is logically flipped.
+  * this depends how we use the optocoupler and may be removed
+  */
+
+
+byte preamble = B10101010;
+byte incoming_preamble = 0;
+
+byte incoming_byte = 0;
+bool incoming_byte_valid = false;
+byte bit_counter = 0;
+byte parity_count = 0;
+
+enum _readstate {
+  PREAMBLE,
+  DATA,
+  PARITY,
+  STOP
+};
+
+volatile enum _readstate readstate = PREAMBLE;
+
+const size_t bufferSize = 128;
+uint8_t buffer[bufferSize];
+RingBuffer ring(buffer, bufferSize);
+
+uint8_t error_buffer[bufferSize];
+RingBuffer error_ring(error_buffer, bufferSize);
+
+boolean on = false;
+volatile boolean stop_req = false;
+
+Packetizer slicer;
+
+//----------------------------------------------
+//----------------------------------------------
+// SETUP
+//----------------------------------------------
+//----------------------------------------------
+void setup() {
+
+  // data input
+  pinMode(2, INPUT); // PD2: ext interrupt
+
+  pinMode(8, OUTPUT); //PB0: pin-interrupt (1-bits)
+  pinMode(9, OUTPUT); //PB1: timer interrupt
+  pinMode(10, OUTPUT); //PB2: bit-output
+  pinMode(11, OUTPUT); //PB3: bit-set toggle
+  pinMode(12, OUTPUT); //PB4: preamble detected
+  pinMode(13, OUTPUT); //PB5: 'a' output
+
+  PORTB = 0;
+
+  // uno running on 16MHz
+  TCCR1A = 0;
+  TCCR1B = 0;
+  OCR1BH = 0;
+  OCR1BL = 0;
+  TCNT1 = 0;
+  
+#ifdef IS_FLIPPED
+  // falling edge
+  EICRA = (1 << ISC01);
+#else
+  // rising edge
+  EICRA = (1 << ISC01) | (1 << ISC00);
+#endif
+
+  
+  Serial.begin(115200);
+  Serial.println("init");
+
+
+  // setup packetizer
+  byte result = slicer.init(128);
+  if (result != pz_noErr)
+  {
+    //error handling
+    Serial.println("error init");
+  }
+
+  // use same start/end conditions as in writer
+  slicer.setStartCondition("sta");
+  slicer.setEndCondition("end");
+
+  // set package-callback
+  slicer.onPacket(myOnPacket);
+
+
+  // start listening for preamble
+  resetPreamble();
+}
+
+
+//----------------------------------------------
+//----------------------------------------------
+// LOOP
+//----------------------------------------------
+//----------------------------------------------
+void loop() {
+
+  while(!error_ring.empty()) {
+    uint8_t i;
+    error_ring.get(i);
+    Serial.print("error: ");
+    Serial.println((int)i);
+  }
+
+  while(!ring.empty()) {
+    uint8_t i;
+    ring.get(i);    
+    slicer.appendData(i);
+  }
+}
+
+
+void myOnPacket(byte* _buffer, unsigned int _bufferSize)
+{
+  // buffer is caller-owned
+  // copy data if you need it later
+
+  Serial.print("buffer: ");
+  Serial.write(_buffer, _bufferSize);
+  Serial.println();
+}
+
+
+//----------------------------------------------
+//----------------------------------------------
+// FUNCTIONS
+//----------------------------------------------
+//----------------------------------------------
+inline void resetPreamble() {
+  // end of line...?
+  // waiting for next preamble
+  stopTimer();
+  stop_req = true;
+  
+  incoming_preamble = 0;
+  readstate = PREAMBLE;
+
+  PORTB &= ~(1 << PB5);
+  PORTB &= ~(1 << PB4);
+
+  startInt0();
+}
+
+inline void readNextByte() {
+  // reset, prepare for next byte
+  incoming_byte = 0;
+  incoming_byte_valid = false;
+  bit_counter = 0;
+  parity_count = 0;
+  
+  // read data
+  readstate = DATA;
+}
+
+
+void setBitValue(boolean _b) {
+
+  // got a bit
+  // if we dot not yet have a preamble add it to preamble
+
+  switch(readstate) {
+    case PREAMBLE:
+      // shift bit into incoming
+      if (_b) {
+        incoming_preamble = (incoming_preamble << 1) + 1;
+      } else {
+        incoming_preamble = (incoming_preamble << 1);
+      }
+
+      if (incoming_preamble == 0) {
+        resetPreamble();
+        return;
+      }
+      
+      if (incoming_preamble == preamble) {        
+        // got a preamble... 
+        PORTB |= (1 << PB4);
+        
+        // getting intersting here.
+        incoming_preamble = 0;
+        readNextByte();
+        
+      } else {
+        PORTB &= ~(1 << PB4);
+      }
+      break;
+
+
+    case DATA:
+      // we already received a preamble, lets add this bit to incoming byte      
+      if (_b) {
+        incoming_byte = (incoming_byte << 1) + 1;
+        parity_count++;
+      } else {
+        incoming_byte = (incoming_byte << 1);
+      }
+
+      if (++bit_counter == 8) {
+        readstate = PARITY;
+      }
+      break;
+      
+
+    case PARITY:
+
+      // 9-th bit = parity bit
+      
+      // parity bit
+#ifdef PARITY_ODD
+      if(parity_count % 2 == 0) {
+        // even number
+        incoming_byte_valid = _b;
+        
+        if (!_b) {
+          // MISSMATCH!
+          //resetPreamble();
+        } else {
+          // we got a full byte...
+          //receivedByte(incoming_byte);
+        }
+      } else {
+        // odd number
+        incoming_byte_valid = !_b;
+        
+        if (_b) {
+          // MISSMATCH!
+          //resetPreamble();
+        } else {
+          // we got a full byte...
+          //receivedByte(incoming_byte);
+        }
+      }
+#else
+      if(parity_count % 2 == 0) {
+        // even number
+        incoming_byte_valid = !_b;
+        if (_b) {
+          // MISSMATCH!
+          //ring.put('.');
+          //resetPreamble();
+        } else {
+          // we got a full byte...
+          //receivedByte(incoming_byte);
+        }
+      } else {
+        // odd number
+        incoming_byte_valid = _b;
+        if (!_b) {
+          // MISSMATCH!
+          //ring.put('.');
+          //resetPreamble();
+        } else {
+          // we got a full byte...
+          //receivedByte(incoming_byte);
+        }
+      }
+#endif
+
+      readstate = STOP;      
+      break;
+
+    case STOP:
+      // 10-th bit = stop bit
+      if (incoming_byte_valid) {
+        ring.put(incoming_byte);
+      } else {
+        slicer.reset();
+        error_ring.put(incoming_byte);
+      }
+      
+      if (_b) {
+        // continue;
+        readNextByte();
+      } else {
+        // ok... transmission stop        
+        resetPreamble();        
+      }
+      
+      break;
+  }
+
+  if (_b) {
+    PORTB |= (1 << PB2);
+  } else {
+    PORTB &= ~(1 << PB2);
+    startInt0();
+  }
+
+  // toggle
+  if (PORTB & (1 << PB3)) {
+    PORTB &= ~(1 << PB3);    
+  } else {
+    PORTB |= (1 << PB3);
+  }
+}
+
+//----------------------------------------------
+//----------------------------------------------
+// FUNCTIONS
+//----------------------------------------------
+//----------------------------------------------
+
+inline void startTimer(int cmp) {
+
+  TCNT1H = 0;
+  TCNT1L = 0;
+  // target = 240  
+  OCR1BH = (byte)(cmp >> 8);
+  OCR1BL = (byte)cmp; //145; // clk/2: 20 us
+
+  // enable timer
+  TIMSK1 |= (1 << OCIE1B);
+  TCCR1B = (1 << CS11);
+
+  //GTCCR &= ~(1 << TSM);
+  stop_req = false;
+}
+
+inline void stopTimer() {  
+  TCCR1B = 0;
+  TIMSK1 &= ~(1 << OCIE1B);
+  TCNT1L = 0;
+  TCNT1H = 0;  
+}
+
+inline void startInt0() {
+  // turn on INT0
+  EIMSK |= (1 << INT0);
+  // turn interrupt flag off
+  EIFR |= (1 << INTF0);
+  PORTB |= (1 << PB0);
+}
+
+inline void stopInt0() {
+  // turn off INT0
+  EIMSK &= ~(1 << INT0);
+  PORTB &= ~(1 << PB0);
+}
+
+
+//----------------------------------------------
+//----------------------------------------------
+// ISR
+//----------------------------------------------
+//----------------------------------------------
+
+//----------------------------------------------
+// pin-interrupt
+ISR(INT0_vect) {
+
+  // rising or falling edge
+  // depends if IS_FLIPPED is defined
+
+  stopTimer();
+  stopInt0();
+
+  // bit length: 200us
+  // expect next bit in 200-300us
+  // if no rising interrupt (1-bit) arrives in time, it is a 0-bit
+  
+  startTimer(590); // 300 us
+
+  // 1-bit
+  setBitValue(true);
+}
+
+
+//----------------------------------------------
+// timer interrupt
+ISR(TIMER1_COMPB_vect) {
+
+  stopTimer();
+
+  // measure...
+  if (PIND & (1 << PD2)) {
+    // high
+#ifdef IS_FLIPPED
+    setBitValue(false);
+#else
+    setBitValue(true);
+#endif
+  } else {
+    // low
+#ifdef IS_FLIPPED
+    setBitValue(true);
+#else
+    setBitValue(false);
+#endif
+  }
+
+  if (!stop_req) {
+    // start timer - next expected bit in 200us
+    startTimer(386); // 200 us
+  }
+  
+  // toggle output for debugging purpose
+  if (PORTB & (1 << PB1) ) {
+    PORTB &= ~(1 << PB1);
+  } else {
+    PORTB |= (1 << PB1);
+  }
+}
+
---
+++ PAN-writer-attiny/PAN-writer-attiny.ino
@@ -0,0 +1,276 @@
+/*
+ * Pinout ATtiny25/45/85
+ *
+ *                                        --------
+ *          (PCINT5/!RESET/ADC0/dW) PB5 -|        |- VCC
+ *   (PCINT3/XTAL1/CLKI/!OC1B/ADC3) PB3 -|        |- PB2 (SCK/USCK/SCL/ADC1/T0/INT0/PCINT2)
+ *    (PCINT4/XTAL2/CLKO/OC1B/ADC2) PB4 -|        |- PB1 (MISO/DO/AIN1/OC0B/OC1A/PCINT1)
+ *                                  GND -|        |- PB0 (MOSI/DI/SDA/AIN0/OC0A/!OC1A/AREF/PCINT0)
+ *                                        --------
+ *
+ */
+
+#include <RingBuffer.h>
+/*
+ * using BufferUtils by Christopher Baker:
+ * https://github.com/bakercp/BufferUtils
+ */
+
+#define PARITY_ODD;
+/*
+ * why ODD:
+ * only zero-value should result in a non-valid byte
+ * this is only possible with odd parity
+ */
+
+
+/*
+ * speed calculations:  
+ * 
+ * 8-bit preamble: 10101010
+ * 
+ * 8-bit data
+ * 1 parity-bit
+ * 1 stop-bit
+ * 
+ * 10 bit / byte
+ *
+ * 200us / bit (defined...)
+ * = 5000 bit / sec (5000 baud)
+ * ~500 byte / sec (- preamble)
+ *
+ *
+ * reaching 9600 baud:
+ * 1 / 9600 = 104,16666666 us / bit
+ */
+
+
+long last_bit_time = 0;
+long last_time = 0;
+
+byte preamble = B10101010;
+int write_count = 7;
+byte outgoing_byte = 0;
+
+byte parity_count = 0;
+byte parity_bit = 0;
+
+const size_t bufferSize = 128;
+uint8_t buffer[bufferSize];
+RingBuffer ring(buffer, bufferSize);
+
+enum _writestate {
+  NONE,
+  PREAMBLE,
+  DATA,
+  PARITY,
+  STOP
+};
+
+volatile enum _writestate writestate = NONE;
+
+void setup() {
+
+  pinMode(1, OUTPUT); // PB1: resonator out
+  
+  // setup timer 1 for PWM
+  // attiny running on 8MHz
+  TCCR1 = (1 << PWM1A) | (1 << COM1A1);
+  OCR1C = 24; //24; // == ~3 us ~= 333 333,333 Hz
+  OCR1A = 12; //12; // 50% duty
+}
+
+void loop() {
+  
+  long _now = micros();  
+  if ((_now - last_bit_time) >= 200) {
+    // every 200 us
+    // TODO: do this in a timer
+    bitOut();
+    last_bit_time = _now;
+  }
+  
+  if ((_now - last_time) >= 200000) {
+
+    // use a start-prefix
+    sendData('s');
+    sendData('t');
+    sendData('a');
+
+    // hello, world!
+    // problems with String on attiny - write out ascii values
+    sendData(104);
+    sendData(101);
+    sendData(108);
+    sendData(108);
+    sendData(111);
+    sendData(44);
+    sendData(32);
+    sendData(119);
+    sendData(111);
+    sendData(114);
+    sendData(108);
+    sendData(100);
+    sendData(33);
+
+    // use a end-prefix
+    sendData('e');
+    sendData('n');
+    sendData('d');
+    
+    last_time = _now;
+  }
+}
+
+//----------------------------------------------
+//----------------------------------------------
+// FUNCTIONS
+//----------------------------------------------
+//----------------------------------------------
+void sendString(String data, int len) {
+  int l = len;//data.length();
+  byte* d = data.c_str();
+
+  for (int i=0; i<l; i++) {
+    sendData(d[i]);
+  }
+}
+
+void sendData(byte data) {
+  ring.put(data);
+}
+
+void sendInt16(int16_t data) {
+  ring.put((byte)(data >> 8));
+  ring.put((byte)data);
+}
+
+inline void pwmOff() {
+  TCCR1 &= ~(1 << CS10);
+}
+
+inline void pwmOn() {
+  TCNT1 = 0;
+  TCCR1 |= (1 << CS10);
+}
+
+
+inline void toPreamble() {
+  write_count = 7;
+  writestate = PREAMBLE;
+}
+
+inline void toParity() {
+  writestate = PARITY;
+}
+
+inline void writeNextByte() {
+  ring.get(outgoing_byte);
+  write_count = 7;
+  parity_count = 0;
+  writestate = DATA;
+}
+
+
+void bitOut() {
+
+  switch(writestate) {
+
+    //----------------------------------------
+    // NONE
+    //----------------------------------------
+    case NONE:
+      // turn off
+      pwmOff();
+            
+      if (!ring.empty()) {        
+        toPreamble();
+      }
+      break;
+
+    //----------------------------------------
+    // PREAMBLE
+    //----------------------------------------
+    case PREAMBLE:
+      if ( ( write_count >= 0 ) && ( preamble & (1 << write_count) ) ) {
+        // 1-bit - value high
+        pwmOn();
+        
+      } else {
+        // 0-bit - value low
+        pwmOff();
+      }
+
+      if (--write_count < 0) {
+        // switch
+        writeNextByte();
+      }
+      break;
+
+    //----------------------------------------
+    // DATA
+    //----------------------------------------
+    case DATA:
+      if ((write_count >= 0) && (outgoing_byte & (1 << write_count))) {
+        // 1-bit - value high
+        pwmOn();
+        parity_count++;
+      } else {
+        // 0-bit - value low
+        pwmOff();
+      }
+
+      if (--write_count < 0) {
+
+        // set parity bit
+#ifdef PARITY_ODD
+      if (parity_count % 2 == 0) {
+        // make it odd
+        parity_bit = 1;
+      } else {
+        // odd numer
+        parity_bit = 0;
+      }
+#else
+      if (parity_count % 2 == 0) {
+        // even number
+        parity_bit = 0;
+      } else {
+        // make it even
+        parity_bit = 1;
+      }      
+#endif
+        // next write parity bit
+        toParity();
+      }
+      break;
+
+    //----------------------------------------
+    // PARITY
+    //----------------------------------------
+    case PARITY:
+      if (parity_bit) {
+        // 1-bit - value high
+        pwmOn();
+      } else {
+        // 0-bit - value low
+        pwmOff();
+      }
+
+      writestate = STOP;
+      break;
+
+    case STOP:
+      if (!ring.empty()) {
+        // more data to send, 1-bit stop-bit
+        pwmOn();
+        writeNextByte();
+      } else {
+        // end of transmission, 0-bit stop bit... ?
+        pwmOff();
+        writestate = NONE;
+      }      
+      break;
+  }
+  
+}
